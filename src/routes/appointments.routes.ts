f8ec03f906f9697af0fa290b286f22113db0f8af
Silva404 <brenosilva0252@gmail.com>
2020-08-24 16:40:04 -0300

    api init

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import { parseISO } from 'date-fns';
 
 const appointmentRouter = Router();
 
 appointmentRouter.post('/', (request, response) => {
   try {
-    return;
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    
+
+    return response.json();
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
