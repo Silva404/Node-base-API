6efe5c61befed149b90fb62febd92e9016a77590
Silva404 <brenosilva0252@gmail.com>
2020-08-28 21:53:27 -0300

    Adde file upload with multer

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UploadConfig from '../config/upload';
 
 const userRouter = Router();
+const upload = multer(UploadConfig);
 
 userRouter.post('/', async (request, response) => {
   try {
@@ -22,5 +26,14 @@ userRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+userRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default userRouter;
