aadd6210c59b8c373a80066d64dd2283457ccd91
Silva404 <brenosilva0252@gmail.com>
2020-10-28 21:31:01 -0300

    feat: add appointments routes

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { parseISO } from 'date-fns';
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import AppointmentRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentRouter = Router();
+
+appointmentRouter.get('/', (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+  const appointments = appointmentsRepository.find();
+
+  return response.json(appointments);
+});
+
+appointmentRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const appointmentService = new CreateAppointmentService();
+
+    const appointment = await appointmentService.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
