923bf831954b69c4c4a5aa8c7033d37bc92d3547
Silva404 <brenosilva0252@gmail.com>
2020-10-29 13:26:48 -0300

    refactor: change Request interface from provider to provider_id

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,12 +4,12 @@ import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 export default class CreateAppointmentService {
-  public async execute({ date, provider }: Request): Appointment {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentRepository = getCustomRepository(AppointmentRepository);
 
     const appointmentDate = startOfHour(date);
@@ -23,7 +23,7 @@ export default class CreateAppointmentService {
     }
 
     const appointment = appointmentRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
