479bd8a5cce73f4f84b596e989b2012ff0ab2618
Silva404 <brenosilva0252@gmail.com>
2020-10-29 13:31:06 -0300

    feat: add index routes Router

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionRouter from './session.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/session', sessionRouter);
+
+export default routes;
