f527fcc5d09b655072b7f6eece342d681a0901c7
Silva404 <brenosilva0252@gmail.com>
2020-10-29 13:26:04 -0300

    feat: add CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { hash } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExist = await usersRepository.findOne({ where: { email } });
+
+    const hashedPassword = await hash(password, 8);
+
+    if (checkUserExist) {
+      throw new Error('Email address already used.');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
