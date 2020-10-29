fc50d9c99db7f01d48d7eee2cb9941db7011735e
Silva404 <brenosilva0252@gmail.com>
2020-10-28 21:59:42 -0300

    refactor: fix uuid returning null by default

--- src/database/migrations/1603925497750-CreateAppointments.ts
+++ src/database/migrations/1603925497750-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1603925497750
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
