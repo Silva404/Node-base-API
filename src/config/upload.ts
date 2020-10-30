79ecef2eebb0ec21d2f475a813ecf69c030b0fd5
Silva404 <brenosilva0252@gmail.com>
2020-10-30 10:30:28 -0300

    refactor: move directory path to a variable and export

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,13 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tmpPath = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: path.resolve(__dirname, '..', '..', 'tmp'),
+
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tmpPath,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('HEX');
       const fileName = `${fileHash}-${file.originalname}`;
