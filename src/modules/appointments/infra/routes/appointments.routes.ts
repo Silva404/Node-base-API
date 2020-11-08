f1a91b96c1ec6074158e9646a80ff8376a14e238
Silva404 <brenosilva0252@gmail.com>
2020-11-08 16:41:06 -0300

    refactor: move business logic to controller layer

--- src/modules/appointments/infra/routes/appointments.routes.ts
+++ src/modules/appointments/infra/routes/appointments.routes.ts
@@ -1,9 +1,9 @@
-import { parseISO } from 'date-fns';
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAutheticated';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppointmentsController from '../controllers/AppointmentsControllers';
+
+const appointmentsController = new AppointmentsController();
 
 const appointmentsRouter = Router();
 
@@ -16,23 +16,6 @@ appointmentsRouter.use(ensureAuthenticated);
 //   return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const appointmentService = container.resolve(CreateAppointmentService);
-
-    const appointment = await appointmentService.execute({
-      provider_id,
-      date: parsedDate,
-    });
-
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
