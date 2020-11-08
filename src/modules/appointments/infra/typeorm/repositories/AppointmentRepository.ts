7bd3879829627ab953f78e13f306b89d2a1e71dd
Silva404 <brenosilva0252@gmail.com>
2020-11-08 15:08:35 -0300

    refactor: implements IAppoitmentsRepositry to TypeORM repository

--- src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -1,11 +1,30 @@
-import { EntityRepository, Repository } from 'typeorm';
-import Appointment from '../models/Appointment';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { getRepository, Repository } from 'typeorm';
+import Appointment from '../entities/Appointment';
 
-@EntityRepository(Appointment)
-export default class AppointmentRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
-    const findAppointment = await this.findOne({ where: { date } });
+export default class AppointmentRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
 
-    return findAppointment || null;
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = await this.ormRepository.findOne({
+      where: { date },
+    });
+
+    return findAppointment;
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ date, provider_id });
+
+    await this.ormRepository.save(appointment);
+    return appointment;
   }
 }
