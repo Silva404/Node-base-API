df6c70dd58f5f037748e09fbb088295bf6e8d5d4
Silva404 <brenosilva0252@gmail.com>
2020-11-08 16:05:42 -0300

    refacotr: fix UserEntitie import path

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,6 +1,6 @@
 import { hash } from 'bcryptjs';
 import { getRepository } from 'typeorm';
-import User from '../models/User';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 interface Request {
   name: string;
